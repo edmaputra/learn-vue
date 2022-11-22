417c07f2bc59b6fdac58ba48bfad56fa7542e0a4
edmaputra <bangun.edma.saputra@gmail.com>
2022-11-22 22:13:26 +0700

    Event and Methods

--- app-event.js
+++ app-event.js
@@ -3,5 +3,13 @@ Vue.createApp({
     return {
       counter: 0,
     };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
   }
 }).mount("#events");
