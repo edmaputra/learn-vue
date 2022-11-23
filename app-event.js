f846a87337b550d3ad8964208d5d23117600846f
edmaputra <bangun.edma.saputra@gmail.com>
2022-11-23 13:08:19 +0700

    Using the Native Event Object

--- app-event.js
+++ app-event.js
@@ -2,6 +2,8 @@ Vue.createApp({
   data() {
     return {
       counter: 0,
+      n: '',
+      fullName: ''
     };
   },
   methods: {
@@ -10,6 +12,12 @@ Vue.createApp({
     },
     decrement(num) {
       this.counter = this.counter - num;
+    },
+    setName(event) {
+      this.n = event.target.value;
+    },
+    setFullName(event, lastName) {
+      this.fullName = event.target.value + ' ' + lastName;
     }
   }
 }).mount("#events");
